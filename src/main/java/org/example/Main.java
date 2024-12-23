9ae87113fe3d49f7186075b20e320c76148d22cc
Adam <adamziv156@gmail.com>
2024-12-23 15:21:31 -0600

    some stuff is working. turns out selenium is not the answer in this case

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,26 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariOptions;
 
 
-
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import java.io.PrintWriter;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -21,17 +28,9 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void scrapeUFCFights(List<Events> events, Set<String> pagesDiscovered, List<String> pagesToScrape) {
-        SafariOptions safariOptions = new SafariOptions();
 
-        WebDriver driver = new SafariDriver(safariOptions);
-        driver.get("http://www.ufcstats.com/statistics/events/completed");
-        try {
-            driver.findElement(By.xpath("/html/body/section/div/div/div/div[2]/div/table/tbody/tr[3]"));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
         if (!pagesToScrape.isEmpty()) {
-            String url = pagesToScrape.get(0);
+            String url = pagesToScrape.getFirst();
             pagesDiscovered.add(url);
 
             Document doc;
@@ -41,26 +40,24 @@ public class Main {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            Elements rows = doc.select("tr.b-statistics__table-row_type_first");
+            Elements rows = doc.selectXpath("/html/body/section/div/div/div/div[2]/div/table/tbody/tr[1]");
             for (Element fight : rows) {
                 Events event = new Events();
 
-                driver.findElement(By.xpath("/html/body/section/div/div/table/tbody/tr[1]"));
-                driver.findElement(By.xpath("/html/body/section/div/div/table/tbody/tr[2]"));
 
-                event.setWin_loss(fight.selectFirst("a.b-flag.b-flag_style_green .b-flag__text").text());
-                event.setFighter(fight.selectFirst("a.b-link.b-link_style_black").text());
-                event.setKd(fight.selectFirst("td.b-fight-details__table-col p.b-fight-details__table-text").text());
-                event.setStr(fight.selectFirst("body > section > div > div > table > tbody > tr:nth-child(1) > td:nth-child(4)").text());
-                event.setTd(fight.selectFirst("body > section > div > div > table > tbody > tr:nth-child(1) > td:nth-child(5)").text());
-                event.setSub(fight.selectFirst("body > section > div > div > table > tbody > tr:nth-child(1) > td:nth-child(6)").text());
-                event.setWeight_class(fight.selectFirst("body > section > div > div > table > tbody > tr:nth-child(1) > td:nth-child(7)").text());
-                event.setMethod(fight.selectFirst("body > section > div > div > table > tbody > tr:nth-child(1) > td:nth-child(8)").text());
-                event.setRound(fight.selectFirst("body > section > div > div > table > tbody > tr:nth-child(1) > td:nth-child(9)").text());
-                event.setTime(fight.selectFirst("body > section > div > div > table > tbody > tr:nth-child(1) > td:nth-child(10)").text());
+                event.setWin_loss(fight.selectXpath("/html/body/section/div/div/table/tbody/tr[1]/td[1]").attr("td"));
+                event.setFighter(fight.selectXpath("/html/body/section/div/div/table/tbody/tr[1]/td[2]").attr("td"));
+                event.setKd(fight.selectXpath("/html/body/section/div/div/table/tbody/tr[1]/td[3]").attr("td"));
+                event.setStr(fight.selectXpath("/html/body/section/div/div/table/tbody/tr[1]/td[4]").attr("td"));
+                event.setTd(fight.selectXpath("/html/body/section/div/div/table/tbody/tr[1]/td[5]").attr("td"));
+                event.setSub(fight.selectXpath("/html/body/section/div/div/table/tbody/tr[1]/td[6]").attr("td"));
+                event.setWeight_class(fight.selectXpath("/html/body/section/div/div/table/tbody/tr[1]/td[7]").attr("td"));
+                event.setMethod(fight.selectXpath("/html/body/section/div/div/table/tbody/tr[1]/td[8]").attr("td"));
+                event.setRound(fight.selectXpath("/html/body/section/div/div/table/tbody/tr[1]/td[9]").attr("td"));
+                event.setTime(fight.selectXpath("/html/body/section/div/div/table/tbody/tr[1]/td[10]").attr("td"));
                 events.add(event);
             }
-            Elements paginations = doc.select("body > section > div > div > div > div.b-statistics__sub-inner > div > table > tbody > tr.b-statistics__table-row_type_first > td:nth-child(1) > i > a");
+            Elements paginations = doc.selectXpath("/html/body/section/div/div/div/div[2]/div/table/tbody/tr[2]/td[1]/i/a");
 
             for (Element pagination : paginations) {
                 String pageUrl = pagination.attr("href");
@@ -73,7 +70,8 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+
 
         List<Events> events = Collections.synchronizedList(new ArrayList<>());
 
@@ -134,30 +132,29 @@ public class Main {
 //        System.out.println(doc + " -> pages scraped " + events.size() + " events");
 
 
-//        File csvFile = new File("Events.csv");
-//        try (PrintWriter pw = new PrintWriter(csvFile)) {
-//            for (Events event : events) {
-//                List<String> row = getStrings(event);
-//                pw.println(String.join(", ", row));
-//            }
-//
-//        }
-//    }
-//
-//    private static List<String> getStrings(Events event) {
-//        List<String> row = new ArrayList<>();
-//        row.add("\"" + event.getWin_loss());
-//        row.add("\"" + event.getFighter());
-//        row.add("\"" + event.getKd());
-//        row.add("\"" + event.getStr());
-//        row.add("\"" + event.getTd());
-//        row.add("\"" + event.getSub());
-//        row.add("\"" + event.getWeight_class());
-//        row.add("\"" + event.getMethod());
-//        row.add("\"" + event.getRound());
-//        row.add("\"" + event.getTime());
-//        return row;
-//    }
+        File csvFile = new File("Events.csv");
+        try (PrintWriter pw = new PrintWriter(csvFile)) {
+            for (Events event : events) {
+                List<String> row = getStrings(event);
+                pw.println(String.join(",", row));
+            }
 
+        }
     }
+
+    private static List<String> getStrings(Events event) {
+        List<String> row = new ArrayList<>();
+        row.add("\"" + event.getWin_loss());
+        row.add("\"" + event.getFighter());
+        row.add("\"" + event.getKd());
+        row.add("\"" + event.getStr());
+        row.add("\"" + event.getTd());
+        row.add("\"" + event.getSub());
+        row.add("\"" + event.getWeight_class());
+        row.add("\"" + event.getMethod());
+        row.add("\"" + event.getRound());
+        row.add("\"" + event.getTime());
+        return row;
+    }
+
 }
